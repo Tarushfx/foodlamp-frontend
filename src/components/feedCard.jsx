acbd930df0182634577480a1b027df3ec8dfd355
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2021-06-22 20:38:07 +0530

    feed card change

--- src/components/feedCard.jsx
+++ src/components/feedCard.jsx
@@ -1,57 +1,46 @@
-import React from "react";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "../css/feed.css";
+import React from 'react';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../css/feed.css';
 
 const FeedCard = (props) => {
-  const postLink = "https://www.reddit.com" + props.link;
+  const postLink = 'https://www.reddit.com' + props.link;
+  let title = props.title;
+  title = title.substring(10);
   // console.log(props.theme);
   return (
-    <div
-      className="card mt-1"
-      style={{
-        backgroundColor: props.theme.secondary,
-        color: props.theme.text,
-      }}
-    >
-      <h6 className="card-title p-1 mb-0">{props.title}</h6>
-      <img
-        className="card-image mw-100 m-1"
-        src={props.imageUrl}
-        label={props.title}
-      ></img>
-
-      <h6 className="card-text p-1 m-0">
-        Posted by <span>u/{props.author}</span> on{" "}
-        {props.time.toString().slice(0, -34)}
-      </h6>
-      <div className="card-footer p-2">
-        <div className="row w-100 m-0 pr-3">
-          <div className="col-6 p-0">
-            <a
-              className="badge badge-secondary"
-              href={postLink}
-              target="_blank"
-            >
-              Go to reddit post
-            </a>
-            <span className="badge upvote">{props.upvotes} upvotes</span>
-          </div>
-          <div className="col p-0"></div>
+    <div className="main-card-outer">
+      <div className="left-img">
+        <img src={props.imageUrl} label={props.title}></img>
+      </div>
+      <div className="right-content">
+        <h6 className="feed-post-title">{title}</h6>
+        <h6 className="post-author">
+          Posted by <span>u/{props.author}</span> on {props.time.toString().slice(0, -34)}
+        </h6>
+        <div>
           <div>
-            <span id="like" onClick={props.handleLike}>
-              {props.data &&
-              props.data.likes &&
-              props.data.likes.includes(
-                JSON.stringify({ name: props.title, link: postLink })
-              ) == true ? (
-                <FavoriteIcon />
-              ) : (
-                <FavoriteBorderIcon />
-              )}
-            </span>
+            <div className="btn-1">
+              <span className="upvotes">{props.upvotes} upvotes</span>
+              <a href={postLink} target="_blank" style={{ textDecoration: 'none' }}>
+                <button>Go to reddit post</button>
+              </a>
+            </div>
+            <div className="col p-0"></div>
+            <div>
+              <span id="like" onClick={props.handleLike}>
+                {props.data &&
+                props.data.likes &&
+                props.data.likes.includes(JSON.stringify({ name: props.title, link: postLink })) ==
+                  true ? (
+                  <FavoriteIcon />
+                ) : (
+                  <FavoriteBorderIcon />
+                )}
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/higherOrderComponents/feedPost.jsx
+++ src/higherOrderComponents/feedPost.jsx
@@ -1,33 +1,30 @@
-import React from "react";
+import React from 'react';
+import './feedpost.css';
 
 const FeedPost = (props) => {
   return (
     <div>
-      <div className="row w-100 m-0">
-        <div className="col col-sm-0 col-md-3 p-0"></div>
-        <div className="col col-sm-12 col-md-5 p-0 col-xs-6">
-          <props.post
-            key={props.index}
-            data={props.data}
-            imageUrl={props.imageUrl}
-            link={props.link}
-            handleLike={props.handleLike}
-            time={props.createTime}
-            title={props.title}
-            theme={props.theme}
-            index={props.index}
-            // feed
-            author={props.author || ""}
-            upvotes={props.upvotes || ""}
-            // recipe
-            ingredients={props.ingredients || ""}
-            healthLabels={props.healthLabels || ""}
-            dietLabels={props.dietLabels || ""}
-            recipeID={props.recipeID || ""}
-            serving={props.serving || ""}
-          />
-        </div>
-        <div className="col col-sm-0 col-md-4 p-0"></div>
+      <div className="feedpost-center">
+        <props.post
+          key={props.index}
+          data={props.data}
+          imageUrl={props.imageUrl}
+          link={props.link}
+          handleLike={props.handleLike}
+          time={props.createTime}
+          title={props.title}
+          theme={props.theme}
+          index={props.index}
+          // feed
+          author={props.author || ''}
+          upvotes={props.upvotes || ''}
+          // recipe
+          ingredients={props.ingredients || ''}
+          healthLabels={props.healthLabels || ''}
+          dietLabels={props.dietLabels || ''}
+          recipeID={props.recipeID || ''}
+          serving={props.serving || ''}
+        />
       </div>
     </div>
   );
